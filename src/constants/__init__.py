af9082fbb7844b73f7f827135fc1aee998870591
Sudarshan Gorakhnath Phule <sgphule@gmail.com>
2025-09-09 03:17:06 +0200

    perform data validation

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,9 +1,16 @@
+import os
+
 DATABASE_NAME = "kfz-mlops-db1"
 COLLECTION_NAME = "kfz-mlops-db-coLL"
 MONGODB_URL_KEY = "MONGODB_URL"
 
 PIPELINE_NAME: str = ""
 ARTIFACT_DIR: str = "artifact"
+
+FILE_NAME: str = "data.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
@@ -14,6 +21,8 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.25
 
-FILE_NAME: str = "data.csv"
-TRAIN_FILE_NAME: str = "train.csv"
-TEST_FILE_NAME: str = "test.csv"
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_REPORT_FILE_NAME: str = "report.yaml"
---
+++ src/utils/main_utils.py
@@ -0,0 +1,13 @@
+import sys
+import yaml
+
+from src.exception import MyException
+
+
+def read_yaml_file(file_path: str) -> dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
+
+    except Exception as e:
+        raise MyException(e, sys) from e
